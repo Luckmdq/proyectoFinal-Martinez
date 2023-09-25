df6cc980cc4d7470e67859101e47a5659cb71867
LucianoMartinez <Lucianom764@gmail.com>
2023-09-25 18:22:28 -0300

    agregado de cta login, login modal

--- js/index.js
+++ js/index.js
@@ -1,205 +1,262 @@
-const iva=0.21;
+const iva = 0.21;
+
+/* CAPTURA DE DISPARADORES DE EVENTOS */
+const btnLogin = document.getElementById("btnLogin")
+const loginModal = document.getElementById("loginModal")
+const closeModal = document.getElementById("closeModal")
+const modalSelect = document.getElementById("usuario")
+const btnMenu = document.getElementById("ctaMenu")
+const menu = document.getElementById("navbar")
 
 
-const pedidos=[
+/* TIPOS DE DATOS  */
+const pedidos = [
 	{
-		id:21,
-		pedido:[
-			{abertura:1,cantidad:2},
-			{abertura:2,cantidad:3},
-		],
-		total:0,
+		id: 21,
+		pedido: [
+			{ abertura: 1, cantidad: 2 },
+			{ abertura: 2, cantidad: 3 },
+		]
 	},
 	{
-		id:1,
-		pedido:[
-			{abertura:2,cantidad:1},
-		],
-		total:0,
+		id: 1,
+		pedido: [
+			{ abertura: 2, cantidad: 1 },
+		]
 	}
 ]
 
-const clientes=[
+const clientes = [
 	{
-		dni:"35764127",
-		nombre:"pedro",
-		direcion:"",
-		compra:[21],
-		vendedor:"1"
+		dni: "35764127",
+		nombre: "pedro",
+		direcion: "",
+		compra: [21],
+		vendedor: "1"
 	},
 	{
-		dni:"33273700",
-		nombre:"jesi",
-		compra:[1],
-		vendedor:"1",
+		dni: "33273700",
+		nombre: "jesi",
+		compra: [1],
+		vendedor: "1",
 	}
 ]
 
-const vendedor=[
+const vendedor = [
 	{
-		id:1,
-		nombre:"pedro",
-		contraseña:"123",
-		clientes:["35764127","33273700"],
+		id: 1,
+		nombre: "pedro",
+		contraseña: "123",
+		clientes: ["35764127", "33273700"],
 	},
 	{
-		id:2,
-		nombre:"jesi",
-		contraseña:"123",
-		clientes:["35764127","33273700"],
+		id: 2,
+		nombre: "jesi",
+		contraseña: "123",
+		clientes: ["35764127", "33273700"],
 	}
 ]
 
-const abertura=[{
-	id:1,
-	nombre:"corrediza",
-	cantidad:0,
-	valor:125,
+const abertura = [{
+	id: 1,
+	nombre: "corrediza",
+	cantidad: 0,
+	valor: 125,
 },
 {
-	id:2,
-	nombre:"abrir",
-	cantidad:0,
-	valor:123,
+	id: 2,
+	nombre: "abrir",
+	cantidad: 0,
+	valor: 123,
 },
 {
-	id:3,
-	nombre:"puerta",
-	cantidad:0,
-	valor:122,
+	id: 3,
+	nombre: "puerta",
+	cantidad: 0,
+	valor: 122,
 }
 ];
 
+
+
+
 /* login de usuario */
-const login=()=>{
+const login = () => {
 	return contraseña === prompt(`ingrese su contraseña`);
 };
 
-const ingresoUsuario=()=>{
-	let pass="";
-	let idVendedor=null;
-	let intentos=0;
-	vendedor.map(elemento=>(
+const ingresoUsuario = () => {
+	let pass = "";
+	let idVendedor = null;
+	let intentos = 0;
+	vendedor.map(elemento => (
 		console.log(`id: ${elemento.id}. Nombre: ${elemento.nombre}`)
 	))
-	idVendedor=parseInt(prompt(`escriba su id`));
-	console.log(vendedor[idVendedor-1].contraseña)
-	while (intentos<3 && pass!==vendedor[idVendedor-1].contraseña) {
-		pass=prompt(`ingrese su contraseña`).toLowerCase();
+	idVendedor = parseInt(prompt(`escriba su id`));
+	console.log(vendedor[idVendedor - 1].contraseña)
+	while (intentos < 3 && pass !== vendedor[idVendedor - 1].contraseña) {
+		pass = prompt(`ingrese su contraseña`).toLowerCase();
 		intentos++;
-	}return pass===vendedor[idVendedor-1].contraseña;
+	} return pass === vendedor[idVendedor - 1].contraseña;
 	//al tener el id de encontrado(valido en el array) solo retornaria true si lo allo si no seria false por lo tanto el ingreso del usuario se revotaria
 }
-const agregarVendedor=()=>{
-	const newVendedor={
-		id:-1,
-		nombre:"",
-		contraseña:"",
-		clientes:[],
+const agregarVendedor = () => {
+	const newVendedor = {
+		id: -1,
+		nombre: "",
+		contraseña: "",
+		clientes: [],
 	}
-	newVendedor.nombre=prompt(`ingrese un nombre`);
-	newVendedor.contraseña=prompt("ingrese una contraseña")
-	newVendedor.id=vendedor.length+1;
+	newVendedor.nombre = prompt(`ingrese un nombre`);
+	newVendedor.contraseña = prompt("ingrese una contraseña")
+	newVendedor.id = vendedor.length + 1;
 	vendedor.push(newVendedor);
 	console.table(vendedor);
 }
 
-const venta=()=>{
+
+const agregarPedido = (dni) => {
+	let cliente = clientes.find((elemento) => elemento.dni === dni)
+	let pedidoNuevo;
+	pedidoNuevo.id = cliente.compra[cliente.compra.length - 1] + pedidos.length
+}
+
+const venta = () => {
 	let tipoVenta;
 	do {
-		tipoVenta=prompt("A-. Ya es un cliente.\nB-. Cliente nuevo.\nS-.Salir");
-		tipoVenta=tipoVenta.toUpperCase()
-		if (tipoVenta==='S') {
+		tipoVenta = prompt("A-. Ya es un cliente.\nB-. Cliente nuevo.\nS-.Salir");
+		tipoVenta = tipoVenta.toUpperCase()
+		if (tipoVenta === 'S') {
 			console.log(`buena suerte`)
 		} else {
-			if(tipoVenta==="A"){
+			if (tipoVenta === "A") {
 				let dniBuscado;
 
 				/* agregar compra con id de cliente ya creado */
-				console.table(`${clientes}`)
-				dniBuscado=prompt(`ingrese el dni del cliente`);
-				
-			}else{
+				console.table(clientes)
+				dniBuscado = prompt(`ingrese el dni del cliente`);
+				agregarPedido(dniBuscado);
+			} else {
 				/* venta a nuevo cliente */
-				if (tipoVenta==="B")
-				
+				if (tipoVenta === "B") {
+
+				}
 			}
 		}
-	} while (tipoVenta!==`S`);
+	} while (tipoVenta !== `S`);
 }
 
 
-const cancelarVenta=()=>{
+const cancelarVenta = () => {
 
 }
 /* 
 modificarVenta() */
 
 /* ingreso de cantidades al stock */
-const ingreso= (condicion,cantidad)=>{
+const ingreso = (condicion, cantidad) => {
 	switch (condicion) {
 		case "A":
-			abertura[0].cantidad+=cantidad;
+			abertura[0].cantidad += cantidad;
 			break;
 		case "B":
-			abertura[1].cantidad+= cantidad;
+			abertura[1].cantidad += cantidad;
 			break;
 		case "C":
-			abertura[2].cantidad+=cantidad;
+			abertura[2].cantidad += cantidad;
 			break;
 	};
 }
 
 /* menu de ingreso  de stock*/
-const entrada = ()=>{	
-	let dato="";
-	let cantidad=0;
+const entrada = () => {
+	let dato = "";
+	let cantidad = 0;
 	do {
-		dato=prompt("ingrese el producto: \n A) Corrediza.\n B) Abrir.\n C)Puerta.\n S) Salir").toUpperCase();
-		if (dato!=="S") {
+		dato = prompt("ingrese el producto: \n A) Corrediza.\n B) Abrir.\n C)Puerta.\n S) Salir").toUpperCase();
+		if (dato !== "S") {
 			//teniendo que validad la cantidad?
-			cantidad=parseInt( prompt("ingrese la cantidad"))
-			ingreso(dato,cantidad)
+			cantidad = parseInt(prompt("ingrese la cantidad"))
+			ingreso(dato, cantidad)
 		}
-	} while (dato !=="S");
+	} while (dato !== "S");
 }
 
-const total=()=>{
-	let acumulado=0;
-	for (let i=0 ; i<abertura.length ; i++) {
-		acumulado+=abertura[i].valor*abertura[i].cantidad;
+const total = () => {
+	let acumulado = 0;
+	for (let i = 0; i < abertura.length; i++) {
+		acumulado += abertura[i].valor * abertura[i].cantidad;
 	}
-	return acumulado*(iva+1);
+	return acumulado * (iva + 1);
 }
 
+const menuUsuario = () => {
+	let opcion
+	do {
+		opcion = prompt(`ingrese la accion \n A.- Agregar vendedor \n B.- Venta \n C.- Cancelar una venta \n D.- Modificar la compra de un cliente \n S.- Salir`).toUpperCase();
+		//agregar vendedor
+		switch (opcion) {
+			case "A":
+				agregarVendedor();
+				break;
+			case "B":
+				venta();
+				break;
+			case "C":
+				cancelarVenta();
+				break;
+			case "D":
+				modificarVenta();
+				break;
+		}
+	} while (opcion !== 'S')
+}
+
+btnLogin.addEventListener("click", (ev) => {
+	loginModal.classList.toggle(`hidden`);
+})
+const showSelects=()=>{
+	vendedor.map(e=>{
+		nuevaOpcion=document.createElement(`option`);
+		nuevaOpcion.value=`${e.id}`;
+		nuevaOpcion.text=`${e.nombre}`;
+		modalSelect.appendChild(nuevaOpcion);
+	})
+}
+
+loginModal.addEventListener("click", e => {
+	switch (e.target.id.toLowerCase()) {
+		case "usuario":
+			if (modalSelect.children.length==1) {showSelects()};
+			break;
+		case "closemodal":
+			loginModal.classList.toggle('hidden');
+			break;
+		case "ingresar":
+			loginModal.classList.toggle('hidden');
+			break;
+		case "cancelar":
+			loginModal.classList.toggle('hidden');
+			break;
+		case "loginmodal":
+			loginModal.classList.toggle('hidden');
+			break;
+	}
+})
+
+loginModal.addEventListener("keydown", e => {
+	if(e.key=="Enter"){
+		e.preventDefault();
+	}
+})
+
+btnMenu.addEventListener("click", () => {
+	menu.classList.toggle("hidden")
+})
 
 /* main */
-/* ingreso valido */
-let usuario=""
-do {
-	usuario=prompt(`desea entrar como vendedor? \n Y/N`).toUpperCase();
-} while (usuario!=="N" && usuario !=="Y");
-if (usuario==="Y"){ 
-	if (ingresoUsuario()){
-		let opcion
-		do {
-			opcion=prompt(`ingrese la accion \n A.- Agregar vendedor \n B.- Venta \n C.- Cancelar una venta \n D.- Modificar la compra de un cliente \n S.- Salir`).toUpperCase();
-			//agregar vendedor
-			switch (opcion) {
-				case "A":
-					agregarVendedor();
-					break;
-				case "B":
-					venta();
-					break;
-				case "C":
-					cancelarVenta();
-					break;
-				case "D":
-					modificarVenta();
-					break;
-			}
-		} while (opcion!=='S');
-	} 
-}else ingresoCliente();
+/* ingreso valido *//* 
+let usuario = ""
+usuario = prompt(`desea entrar como vendedor? \n Y/N`).toUpperCase();
+console.log(usuario === "Y" && ingresoUsuario() )  & menuUsuario();  */
 
