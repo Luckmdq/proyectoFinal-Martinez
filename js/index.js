70021c9c1d68df93b020acffccf009cd281e2c94
LucianoMartinez <Lucianom764@gmail.com>
2023-09-30 08:32:42 -0300

    carga de usuarios al ingresar como vendedor

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ const btnMenu = document.getElementById("ctaMenu")
 const menu = document.getElementById("navbar");
 const formularioIngreso = document.getElementById("formLogin")
 const ctaCarrito=document.getElementById("btnCarrito")
+const viewClientes = document.getElementById("clientes")
 
 
 /* TIPOS DE DATOS  */
@@ -99,8 +100,58 @@ const login = () => {
 };
 
 const showClientes=(idVendedor)=>{
-	let cliente=clientes.filter((client)=>client.vendedor== idVendedor);
+	
 	/* cliente pose todos los clientes del vendedor seleccionado */
+	/* cliente.map((elemento)=>{
+
+	}) */
+	viewClientes.innerHTML=`
+	<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+		<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+			<thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+				<tr>
+					<th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+						Cliente
+					</th>
+					<th scope="col" class="px-6 py-3">
+						CantPedidos
+					</th>
+					<th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+						Monto
+					</th>
+					<th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+						accion
+					</th>
+					</tr>
+			</thead>
+			<tbody id="clientesBody" >
+			</tbody>
+		</table>
+	</div>`
+	let body=document.getElementById("clientesBody");
+	clientes.map(cliente=>{
+		
+		let elemento=(`
+				<tr id="cliente${cliente.id}" class="border-b border-gray-200 dark:border-gray-700">
+					<th scope="row"
+						class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+						${cliente.nombre}
+					</th>
+					<td class="px-6 py-4">
+						${cliente.compra.length}
+					</td>
+					<td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+						$${100.00}
+					</td>
+					<td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+						<div  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Modificar</div>
+						<div  class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Eliminar</div>
+
+					</td>
+				</tr>`
+				);
+			body.insertAdjacentHTML("beforeend",elemento);
+	})
 }
 
 const agregarVendedor = () => {
@@ -193,27 +244,6 @@ const total = () => {
 	return acumulado * (iva + 1);
 }
 
-const menuUsuario = () => {
-	let opcion
-	do {
-		opcion = prompt(`ingrese la accion \n A.- Agregar vendedor \n B.- Venta \n C.- Cancelar una venta \n D.- Modificar la compra de un cliente \n S.- Salir`).toUpperCase();
-		//agregar vendedor
-		switch (opcion) {
-			case "A":
-				agregarVendedor();
-				break;
-			case "B":
-				venta();
-				break;
-			case "C":
-				cancelarVenta();
-				break;
-			case "D":
-				modificarVenta();
-				break;
-		}
-	} while (opcion !== 'S')
-}
 
 btnLogin.addEventListener("click", (ev) => {
 	loginModal.classList.toggle(`hidden`);
